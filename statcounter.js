91aa51b1b55da2e0a93bac1e76ebd0e16bb0f07b
karaoke2026 <aventertainment2026@gmail.com>
2026-08-01 14:14:43 +0530

    Add files via upload

---
+++ statcounter.js
@@ -0,0 +1,17 @@
+<!-- Default Statcounter code for YouTube karaoke and Lyrics
+https://karaoke2026.github.io/ -->
+<script type="text/javascript">
+var sc_project=13339343; 
+var sc_invisible=1; 
+var sc_security="9fb2de63"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="Web Analytics"
+href="https://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="https://c.statcounter.com/13339343/0/9fb2de63/1/"
+alt="Web Analytics"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
