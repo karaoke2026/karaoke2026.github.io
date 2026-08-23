a3856d367b2b10ed118b8b5a5d55cf096ae656d4
karaoke2026 <aventertainment2026@gmail.com>
2026-08-23 22:59:58 +0530

    Add files via upload

---
+++ videos.js
@@ -0,0 +1,5 @@
+const myVideos = [
+    // Correct format (Only the 11 characters after watch?v=)
+    { title: "Malayalam karaoke on Instagram/Twitter", videoId: "dQw4w9WgXcQ" },
+    { title: "test", videoId: "kJQP7kiw5Fk" }
+];
