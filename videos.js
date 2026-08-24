234f89045304d0c1943be898d41ec3f5d2284b89
karaoke2026 <aventertainment2026@gmail.com>
2026-08-24 11:05:49 +0530

    Add files via upload

--- videos.js
+++ videos.js
@@ -1,4 +1,4 @@
 const myVideos = [
-{ title: "important things Keyboard", videoId: "kGH2n2n8UCs", cId: "vbQeNUMAR5CQGQhO" }
+{ title: "important things Keyboard", videoId: "kGH2n2n8UCs", fId: "<iframe width='560' height='315' src='https://www.youtube.com/embed/kGH2n2n8UCs?si=vbQeNUMAR5CQGQhO' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>" }
 
 ];
