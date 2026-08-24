16aa4ec4149fa3fcfab2f9d732ededf6b386d593
karaoke2026 <aventertainment2026@gmail.com>
2026-08-24 10:11:07 +0530

    Add files via upload

--- videos.js
+++ videos.js
@@ -1,5 +1,4 @@
 const myVideos = [
     // Correct format (Only the 11 characters after watch?v=)
-{ title: "important things every Keyboard", videoId: "uh37Kubhd64", sID: "CTbejFvyb_9XFMDR" },
+{ title: 'important things every Keyboard', videoId: 'uh37Kubhd64', iframecode: '<iframe width="560" height="315" src="https://www.youtube.com/embed/uh37Kubhd64?si=jy3idI_26ZOIIoQ7" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>' },
 ];
-
