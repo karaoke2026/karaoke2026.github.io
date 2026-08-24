794869320cfbdac06a1e23653e4b16cb2f25b710
karaoke2026 <aventertainment2026@gmail.com>
2026-08-24 15:03:24 +0530

    Add files via upload

--- videos.js
+++ videos.js
@@ -1,6 +1,6 @@
 const myVideos = [
 { title: "important things Keyboard", videoId: "kGH2n2n8UCs", fId: "<iframe width='560' height='315' src='https://www.youtube.com/embed/kGH2n2n8UCs?si=vbQeNUMAR5CQGQhO' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>" },
-  title: "chanjakkm", videoId: "AuX5ZVYAOQs", fId: "<iframe width='560' height='315' src='https://www.youtube.com/embed/AuX5ZVYAOQs?si=AZynHyUCxhuSKKYy' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>" }
+{ title: "chanjakkm", videoId: "AuX5ZVYAOQs", fId: "<iframe width='560' height='315' src='https://www.youtube.com/embed/AuX5ZVYAOQs?si=AZynHyUCxhuSKKYy' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>" }
 
 ];
 
