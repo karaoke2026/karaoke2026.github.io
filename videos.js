2c43910f8e15e44277c1935d3f76c0a8823ed035
karaoke2026 <aventertainment2026@gmail.com>
2026-08-24 20:43:45 +0530

    Add files via upload

--- videos.js
+++ videos.js
@@ -1,5 +1,5 @@
 const myVideos = [
-{ title: "important things Keyboard", videoId: "kGH2n2n8UCs", fId: "<iframe width='560' height='315' src='https://www.youtube.com/embed/kGH2n2n8UCs?si=vbQeNUMAR5CQGQhO' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>" },
+{ title: "വൈശാഖസന്ധ്യേ നിന്‍ ചുണ്ടിലെന്തേ - Full Karaoke", videoId: "3oVf3k9OCUE", fId: "<iframe width='560' height='315' src='https://www.youtube.com/embed/3oVf3k9OCUE?si=S4TRuePd7A7Nluek' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>" },
 { title: "chanjakkm", videoId: "AuX5ZVYAOQs", fId: "<iframe width='560' height='315' src='https://www.youtube.com/embed/AuX5ZVYAOQs?si=AZynHyUCxhuSKKYy' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share' referrerpolicy='strict-origin-when-cross-origin' allowfullscreen></iframe>" }
 
 ];
